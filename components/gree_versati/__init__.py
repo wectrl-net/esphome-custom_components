fcae494fedf7120f5741da4d3791c75c88bfb9c1
maslyankov <m.maslyankov@me.com>
2024-12-02 17:56:05 +0200

    Add missing imports again

--- components/gree_versati/__init__.py
+++ components/gree_versati/__init__.py
@@ -1,5 +1,5 @@
 import esphome.codegen as cg
-from esphome.components import modbus_controller
+from esphome.components import modbus_controller, number, select, text_sensor, sensor
 from esphome import config_validation as cv
 from esphome.const import CONF_ID
 
