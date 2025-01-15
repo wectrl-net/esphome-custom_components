adb5416d395380fbfdf411ee560953f8440e0b4f
maslyankov <m.maslyankov@me.com>
2025-01-15 18:44:18 +0200

    Remove Privacy Switch implementation from `__init__.py` and add CODEOWNERS file. This change simplifies the component structure by eliminating the previous code related to the `PrivacySwitch` class and its configuration schema, while also establishing ownership for future contributions.

---
+++ components/privacy_switch/switch.py
@@ -0,0 +1,23 @@
+"""Privacy Switch platform."""
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import switch, uart
+from esphome.const import CONF_ID, CONF_UART_ID
+
+DEPENDENCIES = ['uart']
+
+privacy_switch_ns = cg.esphome_ns.namespace('privacy_switch')
+PrivacySwitch = privacy_switch_ns.class_('PrivacySwitch', switch.Switch, cg.Component)
+
+CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(PrivacySwitch),
+    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await switch.register_switch(var, config)
+    await cg.register_component(var, config)
+    
+    uart_component = await cg.get_variable(config[CONF_UART_ID])
+    cg.add(var.set_uart_parent(uart_component)) 
